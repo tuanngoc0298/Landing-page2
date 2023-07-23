e7b68460de69c49b801b95d419113ccc9e7fd0e9
tuanngoc0298@gmail.com <75868787+tuanngoc0298@users.noreply.github.com>
2023-07-23 15:27:42 +0700

    Complete

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ const $$ = document.querySelectorAll.bind(document);
 
 let currentActive = 0;
 let previousActive = 0;
-const navActive = $$(".js-nav-active");
 
+let currentFeedback = 0;
+let previousFeedback = 0;
+
+// Active header
+const navActive = $$(".js-nav-active");
 const activeSong = () => {
     const navItem = navActive[currentActive];
     navItem.classList.add("active");
@@ -17,5 +21,30 @@ navActive.forEach((item, index) => {
             currentActive = index;
         }
         activeSong();
+    });
+});
+
+// Feedback
+const dotActive = $$(".js-dot-active");
+const feedbackItems = $$(".js-fb-item");
+const activeDot = (curr, prev) => {
+    dotActive[curr].classList.add("active");
+    dotActive[prev].classList.remove("active");
+    feedbackItems.forEach((item) => {
+        if (curr != 0) {
+            item.style.transform = `translateX(calc(${curr} * (-100% - 135px))`;
+        } else {
+            item.style.transform = `translateX(0)`;
+        }
+    });
+};
+dotActive[currentFeedback].classList.add("active");
+dotActive.forEach((dot, index) => {
+    dot.addEventListener("click", () => {
+        if (index != currentFeedback) {
+            previousFeedback = currentFeedback;
+            currentFeedback = index;
+            activeDot(currentFeedback, previousFeedback);
+        }
     });
 });
